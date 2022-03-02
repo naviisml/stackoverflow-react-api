8d2a45648c3d3a119cb6142dd29439b8f4544d02
Navi <mailvoornavi@gmail.com>
2022-03-03 00:22:01 +0100

    added simple example for interaction with StackExchange api

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,14 +1,64 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components'
+import axios from 'axios'
 
 import Container from '/src/components/Container'
-//import Search from './Search'
 
-export default function App() {
-	return (
-		<Container>
-			Home
-		</Container>
-	)
+const Paragraph = styled.p`
+	/* ... */
+`;
+
+const Button = styled.button`
+	/* ... */
+`;
+
+export default class Counter extends React.Component {
+	state = {
+		page: 1,
+		items: []
+	}
+
+	componentDidMount() {
+		this.get(this.state.page)
+	}
+
+	get = (page) => {
+		axios.get(`https://api.stackexchange.com/2.3/questions/unanswered?page=` + page + `&pagesize=50&order=desc&sort=activity&site=stackoverflow&tagged=php;sql`).then(res => {
+			const data = res.data.items
+			
+			this.setState({ items: [...this.state.items, ...data] })
+		}).catch((res) => {
+			console.log(res)
+		})
+	}
+
+	increment = () => {
+		this.setState({ page: this.state.page + 1 })
+		this.get(this.state.page)
+	}
+
+	decrement = () => {
+		if ((this.state.page - 1) >= 1) {
+			this.setState({ page: this.state.page - 1 })
+			this.get(this.state.page)
+		}
+	}
+
+	render() {
+		return (
+			<Container>
+				<ul>
+					{
+					this.state.items
+						.map(entry =>
+							<li key={entry.question_id}>{entry.title}</li>
+						)
+					}
+				</ul>
+				<Paragraph>{this.state.page}</Paragraph>
+				<Button onClick={this.increment}>+</Button>
+				<Button onClick={this.decrement}>-</Button>
+			</Container>
+		)
+	}
 }
- 
