33030bbfaaa708bb99cd1493fdc44a12b48727f0
Navi <mailvoornavi@gmail.com>
2022-03-03 04:47:21 +0100

    cleanup

--- src/components/Header/Wrapper.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-const Wrapper = styled.header`
-	display: flex;
-`
-
-export default Wrapper
--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -12,8 +12,6 @@ import { Routes, Route } from 'react-router-dom'
 
 import HomePage from '/src/containers/HomePage'
 import TopicPage from '/src/containers/TopicPage'
-import Header from '/src/components/Header'
-import Footer from '/src/components/Footer'
 
 import GlobalStyle from '/src/global-styles'
 
@@ -25,12 +23,10 @@ export default function App() {
 	 */
 	return (
 		<AppWrapper>
-			<Header />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
       			<Route path="/topic/:id/:slug" element={<TopicPage />} />
 			</Routes>
-			<Footer />
 
 			<GlobalStyle />
 		</AppWrapper>
