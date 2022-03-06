a17f8db6a643e83b6fe4ba46891c3f567327a662
Navi <mailvoornavi@gmail.com>
2022-03-06 15:37:07 +0100

    updated background

--- src/global-styles.tsx
+++ src/global-styles.tsx
@@ -3,21 +3,10 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
 body {
 	background: rgb(220, 220, 220);
-	background: linear-gradient(-45deg, rgba(220, 220, 220,1), rgba(255, 255, 255,1));
-	animation: gradient 15s ease infinite;
+	background: linear-gradient(-45deg, hsla(0, 0%, 0%, .25), transparent);
+	background-position: fixed;
+	background-size: 100%;
 	min-height: 100vh;
-}
-
-@keyframes gradient {
-	0% {
-		background-position: 0% 50%;
-	}
-	50% {
-		background-position: 100% 50%;
-	}
-	100% {
-		background-position: 0% 50%;
-	}
 }
 `
 
