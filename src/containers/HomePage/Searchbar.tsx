9dd0bc5cda5e472b992b62fc65b804b0019d9909
Navi <mailvoornavi@gmail.com>
2022-03-06 14:58:18 +0100

    added new way of removing tags: press backspace twice

--- src/containers/HomePage/Searchbar.tsx
+++ src/containers/HomePage/Searchbar.tsx
@@ -60,30 +60,66 @@ const Close = styled.span`
 
 export default class Searchbar extends React.Component<any, any> {
 	state = {
+		onSearchAction: false,
+		suggestionTimer: null,
 		suggestions: [],
+		selectedTag: -1,
 		tags: [],
 		value: '',
-		timeout: null,
-		onSearchAction: false,
+	}
+
+	/**
+	 * Handle the keyDown event
+	 */
+	handleKeyDown = (event) => {
+		let lastTag = this.state.tags[this.state.tags.length - 1]
+
+		// reset the timer for suggestions
+		if (this.state.suggestionTimer)
+			clearTimeout(this.state.suggestionTimer)
+		
+				
+		// check if the backspace was pressed, and if the length of the input is 0
+		if ((event.keyCode ?? event.charCode) == 8 && event.target.value.length == 0) {
+			// check if the selectedTag was selected, and if the backspace was
+			// pressed again, if true; remove the last tag from the list, and reset
+			// the selected tag
+			if (this.state.selectedTag >= 0 && lastTag) {
+				// remove the selectedTag
+				this.state.tags.splice(this.state.selectedTag, 1)
+
+				// reset the selectedTag
+				this.setSelectedTag(-1)
+
+				// search on the new tagList
+				this.props.onSearchAction(this.state.tags)
+			}
+
+			// check if the selectedTag is NOT set, and if the lastTag exists,
+			// if not; set the selectedTag to the id of the last tag.
+			if (this.state.selectedTag == -1 && lastTag) {
+				// set the selectedTag
+				this.setSelectedTag(this.state.tags.length - 1)
+			}
+		} else {
+			// reset the selected tag
+			this.setSelectedTag(-1)
+		}
 	}
 
 	/**
 	 * Handle the form onChange data
 	 */
 	handleChange = (event) => {
-		let tags = event.target.value.replace(/ /g,'').split(',')
-		let lastTag = tags[tags.length - 1]
+		let tag = event.target.value
 		
-		// Clear the timeout if it exists
-		if (this.state.timeout)
-			clearTimeout(this.state.timeout)
-		
-		// Make a timeout to check if we change the input ('Client throttle')
-		this.state.timeout = setTimeout(() => {
+		// set a timeout, so when a key gets pressed within .5
+		// seconds of each-other, the timer will be reset.
+		this.state.suggestionTimer = setTimeout(() => {
 			// Make sure the search contains atleast 2 characters
-			if ([...lastTag].length >= 2) {
+			if ([...tag].length >= 2) {
 				// Retrieve the tags from the API
-				axios.get(`https://api.stackexchange.com/2.3/tags?pagesize=5&order=desc&sort=popular&inname=${lastTag}&site=stackoverflow`).then(res => {
+				axios.get(`https://api.stackexchange.com/2.3/tags?pagesize=5&order=desc&sort=popular&inname=${tag}&site=stackoverflow`).then(res => {
 					const { data } = res
 					
 					this.setState({ suggestions: data.items })
@@ -92,6 +128,7 @@ export default class Searchbar extends React.Component<any, any> {
 
 					this.setState({ suggestions: [] })
 
+					// oopsie, we have hit a api limit!
 					alert(`Error: ${data.error_message}`)
 				})
 			} else {
@@ -116,20 +153,32 @@ export default class Searchbar extends React.Component<any, any> {
 	 * Add a tag
 	 */
 	addTag = (tagName) => {
-		let searchTag = this.state.suggestions.find((tag) => { return tag.name = tagName })
+		let tags = [...this.state.tags, tagName]
+		
+		// validate tags on existence to save on call cost to the api
+		// NOTE: I've disabled it for now as its subject is unclear, but can be enabled at any time :)
+		/*let searchTag = this.state.suggestions.find((tag) => { return tag.name = tagName })
 
-		if (searchTag && !this.state.tags[tagName]) {
-			let tags = [...this.state.tags, tagName]
+		if (searchTag && !this.state.tags[tagName])
+			return false*/
+		
+		this.setState({
+			tags: tags,
+			value: '',
+			suggestions: [],
+		})
+		
+		// execute the search callback
+		this.props.onSearchAction(tags)
+	}
 
-			this.setState({
-				tags: tags,
-				value: '',
-				suggestions: [],
-			})
-			
-			// execute the search callback
-			this.props.onSearchAction(tags)
-		}
+	/**
+	 * Set the selected tag by index
+	 */
+	setSelectedTag = (index) => {
+		this.setState({
+			selectedTag: index
+		})
 	}
 
 	/**
@@ -154,7 +203,7 @@ export default class Searchbar extends React.Component<any, any> {
 			<Input>
 				<ul>
 					{this.state.tags.map((tag, key) =>
-						<li className="badge mr-2" key={key}>
+						<li key={key} className={(key == this.state.selectedTag) ? 'badge badge-primary mr-2' : 'badge mr-2'}>
 							{tag}
 
 							<Close onClick={() => this.removeTag(tag)}>
@@ -166,7 +215,7 @@ export default class Searchbar extends React.Component<any, any> {
 
 				<Search>
 					<form onSubmit={this.handleSubmit}>
-						<input type="text" placeholder="Search for a tag..." value={this.state.value} onChange={this.handleChange} />
+						<input type="text" placeholder="Search for a tag..." value={this.state.value} onKeyDown={this.handleKeyDown} onChange={this.handleChange} />
 					</form>
 
 					<Dropdown>
